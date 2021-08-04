7bbd19150b2d2b880769116433276c3a18d01eb6
kwb020312 <kwb020312@naver.com>
2021-08-04 21:13:00 +0900

    전적검색 기본 토댇 마련

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Champion from "./component/Champion";
 import ChampionInfo from "./component/ChampionInfo";
+import DetailUser from "./component/DetailUser";
+import SearchUser from "./component/SearchUser";
 
 function App() {
   return (
     <>
       <Route exact={true} path="/" render={() => <Champion />} />
       <Route path="/info" render={() => <ChampionInfo />} />
+      <Route path="/search/:summoner" component={DetailUser} />
+      <Route path="/search" exact={true} component={SearchUser} />
     </>
   );
 }
