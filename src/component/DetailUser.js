493c82f4c6549dd6ea4c9dcf1c120cd4faa04029
kwb020312 <kwb020312@naver.com>
2021-08-05 00:04:02 +0900

    Test Riot Code Upload

--- src/component/DetailUser.js
+++ src/component/DetailUser.js
@@ -3,6 +3,18 @@ import { useEffect, useState } from "react";
 export default function DetailUser({ location: { search } }) {
   const summonerName = search.replace("?userName=", "");
   const [datas, setDatas] = useState([]);
+  /* 
+  
+    {
+        "id": "DZL6MXVX3rgWnDY8OdxlhtzKri0Et2__2-2hkIMlrn86NzuD2PYowPQEmg",
+        "accountId": "5E-0NnMJrYJvvIlx7yRzjrqRzkngAPAkZiJ9AscPklHafc8oZr7MRWxF",
+        "puuid": "XthZME8RfTHGfxHaF8MYkF2nPcPPvj6FB2VsA797GghBzdGE16XIqpdcC3ciGziWFWX-2Wxr-nP6pA",
+        "name": "KimChobby",
+        "profileIconId": 4832,
+        "revisionDate": 1628072553886,
+        "summonerLevel": 86
+    }
+  */
   useEffect(() => {
     async function getData() {
       const CallData = await fetch(
